5bb77e65b49cf67267715feafb091ec34856f554
marti <martinmcm@hotmail.es>
2022-03-22 17:14:17 +0100

    Modificaciones en controladores de Team

--- src/main/java/com/example/programame_project_api/controller/teamController/TeacherController.java
+++ src/main/java/com/example/programame_project_api/controller/teamController/TeacherController.java
@@ -1,4 +1,27 @@
 package com.example.programame_project_api.controller.teamController;
 
+import com.example.programame_project_api.services.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teacher")
 public class TeacherController {
+
+    @Autowired
+    private TeacherService teacherService;
+
+    @GetMapping("/listDataFromTeacher")
+    public ResponseEntity listTeamsAndSponsorFromTeacher(@RequestHeader(name="Authorization") String token) {
+
+        return  teacherService.listDataFromTeacher(token);
+
+    }
+
+
+
 }
--- src/main/java/com/example/programame_project_api/services/TeacherService.java
+++ src/main/java/com/example/programame_project_api/services/TeacherService.java
@@ -1,16 +1,82 @@
 package com.example.programame_project_api.services;
 
+import com.example.programame_project_api.entities.Teacher;
+import com.example.programame_project_api.entities.Team;
+import com.example.programame_project_api.repositories.TeacherRepository;
+import com.example.programame_project_api.security.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class TeacherService {
 
+    @Autowired
+    private TeacherRepository teacherRepository;
 
-//    private ResponseEntity listTeamsAndSponsorsFromTeacher(int teacherId){
-//
-//
-//
-//
-//
-//    }
+    @Autowired
+    private JWTUtil jwtUtil;
 
+
+    public ResponseEntity listDataFromTeacher(String token) {
+
+        try {
+
+            Teacher teacher = teacherRepository.findByEmail(extractEmailFromToken(token));
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(clearValuesFromListData(teacher.getListTeams()));
+
+
+        } catch (Exception e) {
+            return createResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+
+
+    }
+
+
+    private  List clearValuesFromListData(List<Team> listData){
+
+       listData.forEach(team -> {
+            team.setTeacher(null);
+            team.getListSponsors().forEach(sponsor -> {
+                sponsor.setTeam(null);
+                if (sponsor.getSimpleDonation() != null) {
+                    sponsor.getSimpleDonation().setSponsor(null);
+                }
+                if (sponsor.getComplexDonation() != null) {
+                    sponsor.getComplexDonation().setSponsor(null);
+                }
+            });
+
+        });
+
+       return  listData;
+
+    }
+
+
+    private ResponseEntity createResponseEntity(HttpStatus status, String bodyMessage) {
+
+        return ResponseEntity
+                .status(status)
+                .body(bodyMessage);
+
+
+    }
+
+    private String extractEmailFromToken(String token) {
+
+        System.out.println(token);
+        String dato = token;
+        dato = dato.replace("Bearer ", "");
+        System.out.println(dato);
+        return jwtUtil.extractUsername(dato);
+
+
+    }
 }
