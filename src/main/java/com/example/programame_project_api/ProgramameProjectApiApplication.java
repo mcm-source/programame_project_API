b4b3a58bd141b17b70afb87be76ff7c06824ff54
marti <martinmcm@hotmail.es>
2022-03-26 19:12:56 +0100

    Configuración para uso de HTTPS y certificado SSL

--- src/main/java/com/example/programame_project_api/ProgramameProjectApiApplication.java
+++ src/main/java/com/example/programame_project_api/ProgramameProjectApiApplication.java
@@ -1,23 +1,15 @@
 package com.example.programame_project_api;
 
-import com.example.programame_project_api.entities.AuthenticationRequest;
-import com.example.programame_project_api.entities.UserRole;
-import com.example.programame_project_api.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.persistence.Column;
 
 @SpringBootApplication
 public class ProgramameProjectApiApplication {
 
-    
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(ProgramameProjectApiApplication.class, args);
     }
+
+
 
 }
