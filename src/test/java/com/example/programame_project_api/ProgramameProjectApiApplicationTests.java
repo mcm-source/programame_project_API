34b700d99e14ac749a8fd79aa8d04e8a2367eaa3
marti <martinmcm@hotmail.es>
2022-03-29 00:10:15 +0200

    Configuración para Docker

--- src/test/java/com/example/programame_project_api/ProgramameProjectApiApplicationTests.java
+++ src/test/java/com/example/programame_project_api/ProgramameProjectApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProgramameProjectApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
