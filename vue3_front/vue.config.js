a177f03419152644b2c63fbb9cded4189ae1c0be
Pedro Pablo Gómez <programame.organizacion@gmail.com>
2022-04-11 00:31:46 +0200

    [Frontend] Evitadas rutas absolutas en index.html
    Se configura la build de Vue para que en el index.html que se genera
    no añada rutas absolutas que empiecen en el raíz. Así, por ejemplo,
    en lugar de cargar /js/chunk....js cargará js/chunk...js (sin la /
    inicial). Esto permite desplegar la aplicación en rutas que no sean
    la raíz del servidor web.

--- vue3_front/vue.config.js
+++ vue3_front/vue.config.js
@@ -4,8 +4,10 @@ module.exports = defineConfig({
   lintOnSave:false,
   devServer: {
     https: false,
-  }
-
+  },
+  // La aplicación no necesariamente se desplegará en el raíz.                                                                                                                                             
+  // vue-cli-service, por favor, en el index.html que generes en la build                                                                                                                                  
+  // usa rutas relativas al referenciar los recursos, no rutas absolutas                                                                                                                                   
+  // desde el raíz.                                                                                                                                                                                        
+  publicPath: ''
 });
-
-
